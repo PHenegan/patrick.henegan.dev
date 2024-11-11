284f3bde2cc2888f8bc9422e8852a2d66fa8b521
Patrick Henegan <phenegan03@gmail.com>
2024-11-11 18:31:16 -0500

    initial commit

---
+++ routes/+layout.svelte
@@ -0,0 +1,40 @@
+<script>
+  let { children } = $props();
+</script>
+
+<style>
+  .header {
+    display: flex;
+    justify-content: space-between;
+    background-color: #E0E0E0;
+  }
+
+  .header-pages > * {
+    display: inline-flex;
+    justify-content: flex-start;
+  }
+  .header-links > * {
+    display: inline-flex;
+    justify-content: flex-end;
+  }
+
+  ul {
+    flex-direction: row;
+  }
+
+</style>
+
+<div class = "header">
+  <ul class = "header-pages">
+    <li><a href = "/">Home</a></li>
+    <li><a href = "/projects">Projects</a></li>
+    <li><a href = "/experience">Experience</a></li>
+  </ul>
+  <ul class = "header-links">
+    <li><a href = "https://github.com/PHenegan"> GitHub</li>
+    <li><a href = "https://www.linkedin.com/in/patrick-henegan">LinkedIn</li>
+  </ul>
+
+</div>
+
+{@render children()}
---
+++ routes/projects/+page.svelte
@@ -0,0 +1,7 @@
+<h1>Projects</h1>
+
+<p>
+  These are a collection of projects I have worked on outside of work. Some of them were originally
+  projects completed for assignments, but I developed most of them on my own time. I often use
+  personal projects as a way to learn languages or topics in which I'm interested.
+</p>
